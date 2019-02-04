768c47401237a76f6203af4be93e6e1c07004c8c
Daniel Briechle <danbriechle@hotmail.com>
2019-02-04 13:00:58 -0700

    creates test for increment position video model method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Video, type: :model do
   describe 'validations' do
     it { should validate_presence_of :position}
   end
+
+  describe 'class methods' do
+    it '.increment_position' do
+      create(:video, position: 0)
+      create(:video, position: 1)
+      create(:video, position: 2)
+
+      Video.increment_position
+
+      expect(Video.first.position).to eq(1)
+    end
+  end
 end
