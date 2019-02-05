1559bcbfd75bd5a045e711d17571e11b3b20f82a
Nico <nicovigil1@gmail.com>
2019-02-05 13:46:22 -0700

    adds model test for is_user? for displaying a button for add friends next to signed in users

---
+++ spec/features/user/friendship_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "As a user" do
+  context "when I visit my dashboard" do
+    it "there is link to add GH followers as friend on the site if they've connected to GH" do
+        VCR.use_cassette("test relationships") do 
+            dan = create(:user, uid: 123123, first_name: "dan", token: ENV["DAN_GIT_API_KEY"])
+            ian = User.create!(email: 'user1@user.com', first_name: "Namey", last_name: "McNameface", password: "password", role: :default, uid:"168030")
+            mary = User.create!(email: 'user2@user.com', first_name: "Namey_1", last_name: "McNameface", password: "password", role: :default, uid:"40919519")
+
+            # nico = create(:user, uid: 43397150, first_name: "nico", token: ENV["NICO_GIT_API_KEY"])
+            anna = 6686861
+            
+            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(dan)
+            
+            visit dashboard_path 
+            
+            within("#following-#{ian.uid}") do 
+                expect(page).to have_link("Add as Friend")
+            end
+
+            within("#followers-#{mary.uid}") do 
+                expect(page).to have_link("Add as Friend")
+            end
+    
+            within("#following-#{anna}") do 
+                expect(page).to_not have_link("Add as Friend")
+            end
+        end
+    end
+    it "there is no link to add GH followers as friend if they're not connected to GH" do
+      
+    end
+  end
+end
--- spec/models/followers_spec.rb
+++ spec/models/followers_spec.rb
@@ -37,4 +37,19 @@ RSpec.describe "Follower" do
     end
   end
 
+  describe "instance methods" do
+    it '.is_user?' do 
+      gh_resp1 = {"html_url" => "https://www.google.com", "login" => "lmao", "id" => 123}
+      gh_resp2 = {"html_url" => "https://www.google.com", "login" => "lol", "id" => 321}
+      follower = Follower.new(gh_resp1)
+      follower2 = Follower.new(gh_resp2)
+
+      user = create(:user, uid: 123)
+
+      expect(follower.is_user?).to be_truthy
+      expect(follower2.is_user?).to_not be_truthy
+    end
+  end
+  
+
 end
