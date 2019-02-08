9f289c762301a60ac1bd894d96c73717e0b6ecea
Daniel Briechle <danbriechle@hotmail.com>
2019-02-07 19:01:56 -0700

    adds sad path testing to tutorials

--- spec/features/admin/creates_a_new_tutorial_spec.rb
+++ spec/features/admin/creates_a_new_tutorial_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-feature "An admin visiting the admin dashboard" do
-  scenario "can see all tutorials" do
+feature "An admin visiting the admin tutorials new page" do
+  scenario "can create a tutorial with good info " do
     admin = create(:admin)
-
-
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     title = "meaningful name"
@@ -29,6 +27,32 @@ feature "An admin visiting the admin dashboard" do
     # And I should see a flash message that says "Successfully created tutorial."
     expect(page).to have_content("Successfully created tutorial.")
     # expect(page).to have_css(".admin-tutorial-card", count: 2)
+  end
+
+  scenario 'cant create a tutorial with bad info' do
+    admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    title = ""
+    description = "bad description"
+    thumbnail = ""
+
+    # When I visit '/admin/tutorials/new'
+    visit "admin/tutorials/new"
+    # And I fill in 'title' with a meaningful name
+    fill_in "Title", with: title
+    # And I fill in 'description' with a some content
+    fill_in "Description", with: description
+    # And I fill in 'thumbnail' with a valid YouTube thumbnail
+    fill_in "Thumbnail", with: thumbnail
+
+    # And I click on 'Save'
+    click_on 'Save'
+
+    # Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
+    expect(current_path).to eq("admin/tutorials/new")
+    # And I should see a flash message that says "Successfully created tutorial."
+    expect(page).to have_content("Incorrect Info")
   end
 end
 #
